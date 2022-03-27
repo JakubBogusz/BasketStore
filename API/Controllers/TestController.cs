3e7daa0a14b343735f7a9041c4435b451db71ad9
jbogusz23 <jbogusz03@gmail.com>
2022-03-27 17:43:48 +0200

    Add better errors and exception handling to the project

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class TestController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public TestController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var product = _context.Products.Find(42);
+
+            if (product == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var product = _context.Products.Find(42);
+
+            var productToReturn = product.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request on a client side.",
+                401 => "You are not authorized to perform this operation.",
+                404 => "Resource not found.",
+                500 => "Internal server error occured.",
+                _ => null
+            };
+        }
+    }
+}
